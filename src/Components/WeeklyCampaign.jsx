1ec928e8853894ce4233046f5f635742fbf83095
Đoàn Quang Thanh <respectthanh@gmail.com>
2024-11-06 01:05:11 +0700

    update Earn: WeekCampaign

--- src/Components/WeeklyCampaign.jsx
+++ src/Components/WeeklyCampaign.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const WeeklyCampaign = () => {
     return (
-        <div>
-            
+        <div className="relative w-[337px] h-[110px] bg-[#262829] rounded-[20px] left-6">
+            <p className="absolute text-left w-[304px] top-[7px] left-[20px] font-nunito-bold font-bold text-white text-xl tracking-[0] leading-[normal]">
+                Join this week exclusive Campaign
+            </p>
+
+            <div className="relative w-[76px] h-[26px] top-[70px] left-[240px] bg-[#d9d9d9] rounded-[18px]">
+                <div className="text-black text-xl font-nunito-bold font-bold text-center">
+                    Join
+                </div>
+            </div>
         </div>
     );
 };
--- src/Earn.jsx
+++ src/Earn.jsx
@@ -7,7 +7,7 @@ const Earn = () => {
     return (
         <div className="bg-[#1e1e1e] flex flex-row w-screen">
             <div className="bg-[#1e1e1e] overflow-hidden w-screen h-screen relative">
-                <div className="absolute left-[10px] text-[50px] font-baloo font-bold text-white top-[10px]">
+                <div className="absolute left-[10px] text-[40px] font-baloo font-bold text-white top-[10px]">
                     Task
                 </div>
 
@@ -15,18 +15,20 @@ const Earn = () => {
                     <img
                         src={shoppingCart}
                         alt="Shopping cart"
-                        className="w-8 h-8"
+                        className="w-6 h-6"
                     ></img>
-                    <div className="relative top-[2px] font-lato-regular text-white">
+                    <div className="relative top-[2px] font-lato-regular text-white text-[14px] right-[2px]">
                         Shop
                     </div>
                 </div>
                 
-                <div className="absolute left-[30px] top-[80px] font-adlam-display text-white text-[30px]" >
+                <div className="absolute left-[30px] top-[80px] font-adlam-display text-white text-[20px]" >
                     Weekly
                 </div>
 
-                <WeeklyCampaign />
+                <div className="relative top-[120px]">
+                    <WeeklyCampaign />
+                </div>
             </div>
         </div>
     );
