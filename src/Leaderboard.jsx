3568b5255e6ade81ae207cca0833e80de98e480e
Đoàn Quang Thanh <respectthanh@gmail.com>
2024-11-13 14:24:15 +0700

    update: leaderboard temp, fix: icon highlighting

--- src/Leaderboard.jsx
+++ src/Leaderboard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import cute from './assets/cute.png'; 
+import abc from './assets/chibi.png'; 
 
 const Leaderboard = () => {
     return (
-        <div>
-            Leaderboard Page
+        <div className="bg-[#1e1e1e] flex flex-row w-screen">
+            <div className="bg-[#1e1e1e] overflow-hidden w-screen h-[90vh] relative">
+                <div className="text-white font-bold text-3xl relative pt-[15vh] font-auvicwant">Coming soon...</div>
+
+                <img
+                    src={abc}
+                    alt="cute"
+                    className="pt-[5vh] scale-[80%]"
+                ></img>
+            </div>
         </div>
     );
 };
--- src/Learn.jsx
+++ src/Learn.jsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import shoppingCart from './assets/shopping-cart.svg';
 import QuestionSection from './Components/QuestionSection';
 
 const Learn = ({isCampaign, handleClickActive}) => {
